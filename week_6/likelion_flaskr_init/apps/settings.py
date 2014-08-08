9603795a5e3edf71a5ed910642fa50468195e71a
Lea S.S Jeon <maturity0324@gmail.com>
2014-08-09 03:03:09 +0900

    과제절

---
+++ week_6/likelion_flaskr_init/apps/settings.py
@@ -0,0 +1,20 @@
+"""
+settings.py
+
+Configuration for Flask app
+
+"""
+
+
+class Config(object):
+    # Set secret key to use session
+    SECRET_KEY = "likelion-flaskr-secret-key"
+    debug = False
+
+
+class Production(Config):
+    debug = True
+    CSRF_ENABLED = False
+    ADMIN = "maturity0324@gmail.com"
+    SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms:///blog?instance=loyal-semiotics-662:newprojectdb'
+    migration_directory = 'migrations'
